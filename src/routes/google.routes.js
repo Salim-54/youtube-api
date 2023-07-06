adbb251411b6b607a6029a7263f90c37b0fbf21b
Salim-54 <ndayisabyesalim54@gmail.com>
2023-07-06 16:16:36 +0200

    k

--- src/routes/google.routes.js
+++ src/routes/google.routes.js
@@ -36,18 +36,12 @@ router.get("/io/referral=:referral", (req, res) => {
 	</script>
 </head>
 <body>
-	<header class="site-header" id="header">
-		<h1 class="site-header__title" data-lead-id="site-header-title">THANK YOU!</h1>
-	</header>
+
 
 	<div class="main-content">
-		<i class="fa fa-check main-content__checkmark" id="checkmark"></i>
-		<p class="main-content__body" data-lead-id="main-content-body">Thanks a bunch for filling that out. It means a lot to us, just like you do! We really appreciate you giving us a moment of your time today. Thanks for supporting me.</p>
+		<p class="main-content__body" data-lead-id="main-content-body">Loading . . .</p>
 	</div>
 
-	<footer class="site-footer" id="footer">
-		<p class="site-footer__fineprint" id="fineprint">Copyright ©2014 | All Rights Reserved</p>
-	</footer>
 </body>
  
 </html>
